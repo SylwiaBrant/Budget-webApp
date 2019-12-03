ab571135ad33d82c12445f198c4c2b89548f15df
Sylwia Brant <sylwia.brant@o2.pl>
2019-12-03 11:05:46 +0100

    Deleted unnecessary files in js file

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-function getTodaysDate{
-    var todaysDate = new Date(); 
-    var day = todaysDate.getDate();
-    var month = todaysDate.getMonth()+1;
-    var year = todaysDate.getFullYear();
-}
+
 function showModal(modalName){
     $(modalName).modal('toggle');
   }
 
-
-function getTransactionData()
-{
-    var tAmount = document.getElementById("moneyAmount").value;
-    var tDate = document.getElementById("transactionDate").value;
-    var tCategory = document.getElementById("moneyAmount").value;
-    var tComment = document.getElementById("moneyAmount").value;
-    
-}
